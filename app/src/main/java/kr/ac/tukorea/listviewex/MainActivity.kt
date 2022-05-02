5f93b84cbc7812eb0af86e6adcca80f1e137dcea
mi-fasol <100758764+mi-fasol@users.noreply.github.com>
2022-05-03 00:21:06 +0900

    init: listviewEx

--- app/src/main/java/kr/ac/tukorea/listviewex/MainActivity.kt
+++ app/src/main/java/kr/ac/tukorea/listviewex/MainActivity.kt
@@ -1,32 +1,35 @@
 package kr.ac.tukorea.listviewex
 
+import android.graphics.Point
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
+import android.widget.LinearLayout
 import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
 
     var placeList = arrayListOf<PlaceList>(
-        PlaceList("오이도 빨간 등대", "23.7km","oido"),
-        PlaceList("옥구공원", "23.7km","okgu") ,
-        PlaceList("월곶포구", "23.7km","oido") ,
-        PlaceList("시흥 갯골 생태공원", "23.7km","sangtae") ,
-        PlaceList("그린웨이", "23.7km","oido") ,
-        PlaceList("호조벌", "23.7km","dream"),
-        PlaceList("연꽃테마파크", "23.7km","lotus"),
-        PlaceList("물왕저수지", "23.7km","oido"),
-        PlaceList("보통천 자전거길", "23.7km","botong"),
-        PlaceList("시화방조제", "23.7km","sihwa")
+        PlaceList("오이도 빨간 등대", "23.7km", "oido"),
+        PlaceList("옥구공원", "23.7km", "okgu"),
+        PlaceList("월곶포구", "23.7km", "oido"),
+        PlaceList("시흥 갯골 생태공원", "23.7km", "sangtae"),
+        PlaceList("그린웨이", "23.7km", "oido"),
+        PlaceList("호조벌", "23.7km", "dream"),
+        PlaceList("연꽃테마파크", "23.7km", "lotus"),
+        PlaceList("물왕저수지", "23.7km", "oido"),
+        PlaceList("보통천 자전거길", "23.7km", "botong"),
+        PlaceList("시화방조제", "23.7km", "sihwa")
     )
-    lateinit var listView : ListView
+    lateinit var listView: ListView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         listView = findViewById(R.id.listView)
+        listView.isNestedScrollingEnabled = false
         val placeAdapter = MainListAdapter(this, placeList)
         listView.adapter = placeAdapter
-
     }
 }
